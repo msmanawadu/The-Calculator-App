ccd3128481f307fbfe9b671880bab46a2451c9a6
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-09-29 09:12:51 +0530

    Initial Project Template Calibration

---
+++ The Calculator App/The Calculator App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Calculator App
+//
+//  Created by Sandeepa Manawadu on 2020/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
