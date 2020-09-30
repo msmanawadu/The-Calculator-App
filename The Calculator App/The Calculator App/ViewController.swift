ce6a21d31909d6dcbba0d47ffaeee6cab408bc08
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-09-30 11:14:25 +0530

    Show calculated result upon equals button press

--- The Calculator App/The Calculator App/ViewController.swift
+++ The Calculator App/The Calculator App/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
     
     var labelString: String = "0"
     var currentMode: modes = .notSet
-    var savedNum: Int = 0
+    var savedNum: Int = 0 // holds first numeric value entered before any operator button tap
     var lastButtonWasOperator: Bool = false
     
     
@@ -41,8 +41,39 @@ class ViewController: UIViewController {
         
     }
     
-    
+    // method where all calculation happens
     @IBAction func didPressEquals(_ sender: Any) {
+        // make sure that we can convert the labelString to Int
+        
+        guard let labelInt: Int = Int(labelString) else {
+            label.text = "Error"
+            return
+            // labelInt - holds second numeric value entered after any operator button tap
+        }
+        
+        /* make sure the current mode is an operator mode. If mode is notSet OR last button pressed was a operator button, backout from method*/
+        if(currentMode == .notSet || lastButtonWasOperator){
+            return
+        }
+        
+        // evaluate the current operator mode
+        if(currentMode == .addition){
+            savedNum += labelInt
+        } else if(currentMode == .substraction) {
+            savedNum -= labelInt
+        }
+        
+        // reset current operator mode to notSet
+        currentMode = .notSet
+        
+        // update the label with calculated result
+        labelString = "\(savedNum)"
+        updateText()
+        
+        /* since the last button tapped was equals (=) button, which is an operator button */
+        lastButtonWasOperator = true
+        // make sure user presses numeric buttons next
+        
         
     }
     
@@ -54,14 +85,14 @@ class ViewController: UIViewController {
         savedNum = 0
         lastButtonWasOperator = false
         
-        //reset label text to 0
+        // reset label text to 0
         label.text = "0"
         
     }
     
     
     @IBAction func didPressNumber(_ sender: UIButton) {
-        //accept the numeric input and make sure that there is textual input
+        // accept the numeric input and make sure that there is textual input
         
         guard let stringValue: String = sender.titleLabel?.text else {
             label.text = "Error"
@@ -69,52 +100,52 @@ class ViewController: UIViewController {
         }
         
         if (lastButtonWasOperator) {
-            //if true, the last button tapped, is no longer an operator button
+            // if true, the last button tapped, is no longer an operator button
             lastButtonWasOperator = false
             
-            //reset the label string
+            // reset the label string
             labelString = "0"
             
         }
         
-        //append and set the labelString
+        // append and set the labelString
         labelString = labelString.appending(stringValue)
         
-        //handle possible preceeding 0's issue by, to Integer type casting
+        // handle possible preceeding 0's issue by, to Integer type casting
         updateText()
         
     }
     
     
     func updateText() {
-        //make sure that we can convert the labelString to Int
+        // make sure that we can convert the labelString to Int
         
         guard let labelInt: Int = Int(labelString) else {
             label.text = "Error"
             return
         }
         
-        //capturing every numeric input until user presses an operator button
+        // capturing every numeric input until user presses an operator button
         if (currentMode == .notSet) {
             savedNum = labelInt
         }
         
-        //set the label text
+        // set the label text
         label.text = "\(labelInt)"
         
     }
     
-    //changes operator modes upon operator button taps
+    // changes operator modes upon operator button taps
     func changeModes(newMode: modes){
-        //ignore operator button taps before any number inputs
+        // ignore operator button taps before any number inputs
         if(savedNum == 0){
             return
         }
         
-        //if there is a number input, before an operator button tap
+        // if there is a number input, before an operator button tap
         currentMode = newMode
         
-        //update that the last button tapped was an operator button
+        // update that the last button tapped was an operator button
         lastButtonWasOperator = true
     }
     
