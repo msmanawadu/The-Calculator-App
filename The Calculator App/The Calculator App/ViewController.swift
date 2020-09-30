c209b0b9f7ca6f6d98186f4aa5bd124c8aa253d6
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-09-30 09:04:31 +0530

    Connected the UI elements to code and display tapped numbers in the label

--- The Calculator App/The Calculator App/ViewController.swift
+++ The Calculator App/The Calculator App/ViewController.swift
@@ -7,12 +7,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum modes {
+    case notSet
+    case addition
+    case substraction
+    case multiplication
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var labelString: String = "0"
+    var currentMode: modes = .notSet
+    var savedNum: Int = 0
+    var lastButtonWasMode: Bool = false
+    
+    
+    @IBAction func didPressPlus(_ sender: Any) {
+        
     }
+    
+    
+    @IBAction func didPressMinus(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func didPressMultiply(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func didPressEquals(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func didPressClear(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func didPressNumber(_ sender: UIButton) {
+        //accept the numeric input and make sure that there is textual input
+        
+        guard let stringValue: String = sender.titleLabel?.text else {
+            label.text = "Error"
+            return
+        }
+        
+        labelString = labelString.appending(stringValue)
+        
+        //handle the possible preceeding 0's issue
+        updateText()
+    }
+    
+    
+    func updateText() {
+        //make sure that we can convert the labelString to Int
+        
+        guard let labelInt: Int = Int(labelString) else {
+            label.text = "Error"
+            return
+        }
+        
+        //set the label text
+        label.text = "\(labelInt)"
+    }
+    
+    func changeModes(newMode: modes){
+        
+    }
+    
 
 
 }
